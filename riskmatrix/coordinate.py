ea93e3079b870393f48d6ada48626bf293407f4c
lonkeknol <lonkeknol@gmail.com>
2019-07-15 21:50:16 +0200

    Simplified ordering of coordinates and added location_equals method to find out if a coordinate points to the exact same location. Equality will now also return true if the coordinate is in an equivalent position

--- riskmatrix/coordinate.py
+++ riskmatrix/coordinate.py
@@ -4,15 +4,24 @@ from .axis import AxisPoint
 
 
 class Coordinate:
-    """
-    A collection of AxisPoints to represent a location in a matrix.
-    """
+    """A collection of AxisPoints to represent a location in a matrix."""
 
-    def __init__(self, points: Tuple[AxisPoint, AxisPoint]) -> None:
+    def __init__(self, points: Tuple[AxisPoint]) -> None:
+        if any(p.axis is None for p in points):
+            raise ValueError(
+                "There is at least one point which is not tied to an axis."
+            )
+
+        if any(p.axis.matrix is None for p in points):
+            raise ValueError(
+                "There is at least one point with an axis that is not tied to a matrix."
+            )
+
         if len(set(p.axis.matrix for p in points)) != 1:
             raise ValueError(
                 "The points in this coordinate are not all from the same matrix."
             )
+
         self.points = points
         self.matrix = None
 
@@ -21,7 +30,7 @@ class Coordinate:
 
     def __str__(self) -> str:
         """ The str format is also used for determining equality with coordinates in string format """
-        return f'{"".join(str(p.code) for p in self)}'
+        return f'{"".join(str(p.code) for p in self.points)}'
 
     def __getitem__(self, key: int) -> AxisPoint:
         return self.points[key]
@@ -29,12 +38,26 @@ class Coordinate:
     def __hash__(self) -> int:
         return hash(self.points)
 
-    def __eq__(self, other: Union[Coordinate, str]) -> bool:
-        """ Support equality for both string and Coordinate format """
-        if type(other) is str:
-            return str(self) == other
-        return self.points == other.points
+    def __eq__(self, other: Coordinate) -> bool:
+        return sum(p.value for p in self.points) == sum(p.value for p in other.points)
+
+    def __lt__(self, other: Coordinate) -> bool:
+        return sum(p.value for p in self.points) < sum(p.value for p in other.points)
 
     @property
     def value(self) -> int:
-        return sum(point.value for point in self.points)
+        return sum(p.value for p in self.points)
+
+    def location_equals(self, other: Union[Coordinate, str]) -> bool:
+        """Check if two Coordinates are pointing to the same location.
+
+        Support equality for both string and Coordinate format.
+
+        :param other: A Coordinate object or string representation.
+        :type other: Union[Coordinate, str]
+        :return: Return True if coordinate location is equal, otherwise return False.
+        :rtype: bool
+        """
+        if type(other) is str:
+            return str(self) == other
+        return self.points == other.points
