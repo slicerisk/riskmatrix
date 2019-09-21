14ce07c8a2bff01bae8a1b63a2119274938b7afc
lonkeknol <lonkeknol@gmail.com>
2019-09-21 16:52:35 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/slicerisk/riskmatrix",
-    version="0.0.8",
+    version="0.0.9",
     zip_safe=False,
 )
